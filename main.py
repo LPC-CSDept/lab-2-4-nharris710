e3817f4cff12eb4c9e2e8631bc6d41e73b91cddc
nharris710 <noaharris248@gmail.com>
2023-02-20 23:55:25 -0800

    string

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+    sub1 = 'Python Programing'
+    sub2 = 'Python Programing'
+    print(sub1[7:])
+    print(sub2[0:7])
+    print(sub1[7:] + sub2[0:7])
+    
 
 
 if __name__ == '__main__':
