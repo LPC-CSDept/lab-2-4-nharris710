00e5127fa2d02cc620aa04fe6331d1f9e197d0a3
nharris710 <noaharris248@gmail.com>
2023-02-21 23:45:28 -0800

    string

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    sub1 = 'Python Programing'
-    sub2 = 'Python Programing'
+    sub1 = 'Python Programming'
+    sub2 = 'Python Programming'
     print(sub1[7:])
     print(sub2[0:7])
-    print(sub1[7:] + sub2[0:7])
+    print(sub1[7:], sub2[0:7])
     
 
 
